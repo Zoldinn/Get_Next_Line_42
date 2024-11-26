feaf6cae910797d1a8daf02a18fdd37ae23a7c5d
lefoffan - leon <lefoffan@student.42perpignan.fr>
2024-11-26 17:56:10 +0100

    first commit

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lefoffan <lefoffan@student.42perpignan.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/26 14:37:50 by lefoffan          #+#    #+#             */
+/*   Updated: 2024/11/26 17:12:21 by lefoffan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <fcntl.h>
+
+char	*get_next_line(int fd);
+
+#endif
